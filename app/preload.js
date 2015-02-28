492a4d0537bfce6f0292de946353aa4b66498fb4
Alex Bardanov <dnbard@gmail.com>
2015-02-28 19:28:29 +0200

    project restart

--- app/preload.js
+++
@@ -1,8 +0,0 @@
-define([], function(){
-    function Preload(game){
-        game.load.image('backdrop', 'assets/pics/remember-me.jpg');
-        game.load.image('card', 'assets/sprites/mana_card.png');
-    }
-
-    return Preload;
-});
