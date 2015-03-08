e0fbc0f177258b17ea39be33549c159fc3b093a3
Alex Bardanov <dnbard@gmail.com>
2015-03-08 22:37:10 +0200

    bootstrap changed objects

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,24 @@
+var _ = require('lodash');
+
+function checkProperty(entity, fieldValue, fieldName){
+    var isModified, arrayOpenResult;
+
+        if (typeof fieldValue === 'object' && typeof entity[fieldName] === 'object'){
+            arrayOpenResult = _.map(fieldValue, (value, name) => checkProperty(entity[fieldName], value, name));
+            isModified = _.reduce(arrayOpenResult, (res, curr) => res || curr);
+        } else if (entity[fieldName] !== fieldValue){
+            entity[fieldName] = fieldValue;
+            isModified = true;
+        }
+
+        return isModified;
+}
+
+function checkEntity(entity, entityValue){
+    var arrayOpenResult = _.map(entityValue, (fieldValue, fieldName) => checkProperty(entity, fieldValue, fieldName));
+    return _.reduce(arrayOpenResult, (res, curr) => res || curr);
+}
+
 function bootstrapDataInDatabase(model, data, field, id){
     var query = {};
     query[field] = id;
@@ -5,13 +26,19 @@ function bootstrapDataInDatabase(model, data, field, id){
     model.findOne(query)
         .exec()
         .then((entity) => {
+            var modified = false;
             if(entity){
-                //TODO: check with data and modify if needed
+                modified = checkEntity(entity, data);
             } else {
                 entity = new model(data);
+                modified = true;
+            }
+
+            if (modified){
+                console.log('save');
                 entity.save();
             }
-        })
+        });
 }
 
 exports.classes = function(){
